13b53b79b538f56fffaee3423bcb5daa66718e4e
tejimil <anutko1996@rambler.ru>
2019-10-27 11:51:30 +0300

    Задание №5

---
+++ src/task_5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class task_5 {
+    public static void main(String[] args) {
+        System.out.println("Введите число:");
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        System.out.println(a+"*1="+(a*1));
+        System.out.println(a+"*2="+(a*2));
+        System.out.println(a+"*3="+(a*3));
+        System.out.println(a+"*4="+(a*4));
+        System.out.println(a+"*5="+(a*5));
+        System.out.println(a+"*6="+(a*6));
+        System.out.println(a+"*7="+(a*7));
+        System.out.println(a+"*8="+(a*8));
+        System.out.println(a+"*9="+(a*9));
+        System.out.println(a+"*10="+(a*10));
+
+    }
+}
