a6c02c830fa6f4205d116da4168b2deec9000b45
tejimil <anutko1996@rambler.ru>
2019-10-27 19:35:16 +0300

    Задание №14

---
+++ src/task_14.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class task_14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число");
+        String s = in.nextLine();
+        int x=Integer.parseInt(s);
+        double y=(double)x;
+        System.out.println(s);
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
