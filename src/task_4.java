e9306e5d2933f58d98afd6a69259802e8a59fdc6
tejimil <anutko1996@rambler.ru>
2019-10-27 11:36:43 +0300

    Задание №4

---
+++ src/task_4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class task_4 {
+    public static void main(String args[]) {
+        int a=0;
+        int o=1;
+       Scanner in = new Scanner(System.in);
+       String b = in.nextLine();
+       for(int i=0;i<b.length();i++){
+            if(b.charAt(i)>'1') {
+                System.out.println("Неверный формат.");
+                System.exit(0);
+            }
+       }
+       for(int j=0;j<b.length();j++) {
+           if(b.charAt(j)=='1'){
+               o <<= (b.length() - j - 1);
+               a=a|o;
+               o=1;
+           }
+       }
+       System.out.println(b +" в двоичном формате = "+a+" в десятичном формате");
+    }
+}
