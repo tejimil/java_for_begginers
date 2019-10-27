b540423486b25c7775dacc04319574c3c1d14c35
tejimil <anutko1996@rambler.ru>
2019-10-27 18:54:52 +0300

    Задание №11

---
+++ src/task_11.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class task_11 {
+    public static void main(String[] args) {
+        System.out.println("Введите строку");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.printf("Ваша строка - %s", s);
+    }
+}
